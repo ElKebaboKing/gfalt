7167595845b4748a2efcbcfbc0c5777edfc5969b
BeCrazy <elkebaboking@gmail.com>
2024-06-16 23:22:21 +0200

    Fix package to work with web3.

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,31 @@
-const { override, addWebpackModuleRule } = require('customize-cra');
+const webpack = require("webpack");
 
-module.exports = override(
-    addWebpackModuleRule({
-        test: /\.js$/,
-        enforce: 'pre',
-        use: ['source-map-loader'],
-        exclude: [/node_modules\/web3/],
-    })
-);
+module.exports = function override(config) {
+    const fallback = config.resolve.fallback || {};
+
+    Object.assign(fallback, {
+        // ENABLE OR DISABLE YOUR POLYFILLS HERE
+        stream: require.resolve("stream-browserify"),
+        crypto: require.resolve("crypto-browserify"),
+        assert: require.resolve("assert/"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify")
+    });
+
+    config.resolve.fallback = fallback;
+    config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+            Buffer: ["buffer", "Buffer"],
+        }),
+    ]);
+    config.resolve.extensions.push(".mjs");
+    config.module.rules.push({
+        test: /\.m?js/,
+        resolve: {
+            fullySpecified: false,
+        },
+    });
+
+    return config;
+};
